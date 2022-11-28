09293e939e294fd9a1ddc303892405062be32b33
Manoj R <manojr.kalpana@gmail.com>
2022-11-28 12:02:45 +0530

    Implemented migration for cinema system

--- express_typeorm/src/database/migrations/1663877813247-CinemaSystem.ts
+++ express_typeorm/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -1,4 +1,9 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
 
 export class CinemaSystem1663877813247 implements MigrationInterface {
   /**
@@ -31,7 +36,277 @@ export class CinemaSystem1663877813247 implements MigrationInterface {
    * As a cinema owner I dont want to configure the seating for every show
    */
   public async up(queryRunner: QueryRunner): Promise<void> {
-    throw new Error('TODO: implement migration in task 4');
+    await queryRunner.createTable(
+      new Table({
+        name: "user",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "integer",
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "name", type: "varchar" },
+          { name: "email", type: "varchar" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: "show_room",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "integer",
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "name", type: "varchar" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: "film",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "integer",
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "filmName", type: "varchar" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: "seat_type",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "integer",
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "name", type: "varchar" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: "show_room_seat_mapping",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "integer",
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "showRoomId", type: "integer", isNullable: true },
+          { name: "seatNumber", type: "varchar" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "show_room_seat_mapping",
+      new TableForeignKey({
+        columnNames: ["showRoomId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "show_room",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: "show_mapping",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "integer",
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "showRoomId", type: "integer", isNullable: true },
+          { name: "filmId", type: "integer", isNullable: true },
+          { name: "start", type: "timestamp" },
+          { name: "end", type: "timestamp" },
+          { name: "status", type: "varchar" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "show_mapping",
+      new TableForeignKey({
+        columnNames: ["showRoomId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "show_room",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "show_mapping",
+      new TableForeignKey({
+        columnNames: ["filmId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "film",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: "pricing",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "integer",
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "showMappingId", type: "integer", isNullable: true },
+          { name: "seatTypeId", type: "integer", isNullable: true },
+          { name: "seatPriceType", type: "varchar" },
+          { name: "seatPriceValue", type: "varchar" },
+          { name: "status", type: "varchar" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "pricing",
+      new TableForeignKey({
+        columnNames: ["showMappingId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "show_mapping",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "pricing",
+      new TableForeignKey({
+        columnNames: ["seatTypeId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "seat_type",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: "booking",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "integer",
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "userId", type: "integer", isNullable: true },
+          { name: "showMappingId", type: "integer", isNullable: true },
+          { name: "showRoomSeatingId", type: "integer", isNullable: true },
+          { name: "pricingId", type: "integer", isNullable: true },
+          { name: "status", type: "varchar" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "booking",
+      new TableForeignKey({
+        columnNames: ["userId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "user",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "booking",
+      new TableForeignKey({
+        columnNames: ["showMappingId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "show_mapping",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "booking",
+      new TableForeignKey({
+        columnNames: ["showRoomSeatingId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "show_room_seat_mapping",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "booking",
+      new TableForeignKey({
+        columnNames: ["pricingId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "pricing",
+        onDelete: "CASCADE",
+      })
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
